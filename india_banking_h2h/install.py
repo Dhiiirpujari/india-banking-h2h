f13214955d78cd09154e4206871bce7257c77234
Dhiiirpujari <dhirajpujari2000@gmail.com>
2026-03-10 12:55:56 +0530

    add: Prints on Installation for creation fo Banks

--- india_banking_h2h/install.py
+++ india_banking_h2h/install.py
@@ -14,6 +14,7 @@ def after_install():
 def create_default_banks():
 	click.echo(" -> Creating Default Banks")
 	for bank in STD_BANK_LIST:
+		print("bank****", bank)
 		if not frappe.db.exists("Bank", bank):
 			bank_doc = frappe.new_doc("Bank")
 			bank_doc.bank_name = bank
@@ -82,4 +83,5 @@ def create_bank_doctype():
 				},
 			],
 		}
+		print("doc****", doc)
 		frappe.call("frappe.client.insert", doc=doc)
